230fe572fccf791f9dc31e96e661dcf5a65ca1c8
Rayness <mr.konan.tub@gmail.com>
2024-12-01 17:36:26 +0300

    add lounch.bat - new run file and more

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import subprocess
 import sys
 import yt_dlp
 from pathlib import Path
-import mp4
-import mp3
+
 folder = "downloads"
 
 ffmpeg_dir = Path("ffmpeg")  # Директория, куда распакуем FFmpeg
@@ -84,29 +83,81 @@ def download_audio_as_mp3(url, output_folder):
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
+def download_video(url, resolution, output_folder):
+    ydl_opts = {
+        'format': f'bestvideo[height<={resolution}]+bestaudio/best[height<={resolution}]',
+        'outtmpl': os.path.join(output_folder, '%(title)s.%(ext)s'),
+        'merge_output_format': 'mp4',
+        'quiet': False,  # Показывает прогресс в консоли
+        'socket_timeout': 60,
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+def video_settings():
+    print("Выберите предпочтительное разрешение")
+    print("")
+    print("[1] - 720p || HD")
+    print("[2] - 1080p || FHD")
+    print("[3] - 1440p || 2K - если поддерживается видео")
+    print("[4] - 2160p || 4K - если поддерживается видео")
+    try:
+        choice = int(input("Введите цифру для продолжения: "))
+        if choice == 1:
+            print("Загрузчик видео")
+            link = input("Вставьте ссылку на видео: ")
+            download_video(link, "720p", folder)
+            open_folder(folder)
+        elif choice == 2:
+            print("Загрузчик видео")
+            link = input("Вставьте ссылку на видео: ")
+            download_video(link, "1080p", folder)
+            open_folder(folder)
+        elif choice == 3:
+            print("Загрузчик видео")
+            link = input("Вставьте ссылку на видео: ")
+            download_video(link, "1440p", folder)
+            open_folder(folder)
+        elif choice == 4:
+            print("Загрузчик видео")
+            link = input("Вставьте ссылку на видео: ")
+            download_video(link, "2160p", folder)
+            open_folder(folder)
+        else:
+            print("Неверный выбор. Попробуйте снова")
+    except ValueError:
+        print("Пожалуйста, введите только число!")
 
 def main():
-    print("\033[31mДобро пожаловать в YouTube Downloader\033[0m")
-    print("")
-    print("\033[32mДля продолжения выберите пункт меню вводя нужные цифры на клавиатуре\033[0m")
-    print("------------------------------------------------------------------------")
-    print("\033[34m[1]\033[0m - Скачать аудио в формате .mp3 --- Максимальное качество звука")
-    print("\033[34m[2]\033[0m - Скачать видео в формате .mp4 --- HD - 4K")
-    print("------------------------------------------------------------------------")
-    print("\033[31m[0]\033[0m - Выход из программы")
-    print("")
-    try:
-        choice = int(input("Введите цифру: "))
-        if choice == 1:
-            print("Загрузчик аудио")
-            link = input("Вставьте ссылку на видео: ")
-            download_audio_as_mp3(link, folder)
-        elif choice == 0:
-            exit()
-        else:
-            print("Неверный выбор. Попробуйте снова.")
-    except ValueError:
-        print("Пожалуйста, введите только число.")
+    while True:
+        print("")
+        print("\033[31mДобро пожаловать в YouTube Downloader\033[0m")
+        print("")
+        print("\033[32mДля продолжения выберите пункт меню вводя нужные цифры на клавиатуре\033[0m")
+        print("------------------------------------------------------------------------")
+        print("\033[34m[1]\033[0m - Скачать аудио в формате .mp3 --- Максимальное качество звука")
+        print("\033[34m[2]\033[0m - Скачать видео в формате .mp4 --- HD - 4K")
+        print("------------------------------------------------------------------------")
+        print("\033[31m[3]\033[0m - Настройки")
+        print("------------------------------------------------------------------------")
+        print("\033[31m[0]\033[0m - Выход из программы")
+        print("")
+        try:
+            choice = int(input("Введите цифру: "))
+            if choice == 1:
+                print("Загрузчик аудио")
+                link = input("Вставьте ссылку на видео: ")
+                download_audio_as_mp3(link, folder)
+                open_folder(folder)
+            elif choice == 2:
+                video_settings()
+            elif choice == 0:
+                break
+            else:
+                print("Неверный выбор. Попробуйте снова.")
+        except ValueError:
+            print("Пожалуйста, введите только число.")
 
 if __name__ == "__main__":
     main()
