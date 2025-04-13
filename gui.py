375d9db2ec22b9042657b908e6bd214977514a79
Rayness <mr.konan.tub@gmail.com>
2025-04-14 01:08:41 +0300

    Copyright added

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2025 Andrei Mamonov
+# Copyright (C) 2025 Rayness
 # This program is free software under GPLv3. See LICENSE for details.
 
 import os
--- update.py
+++ update.py
@@ -1,3 +1,6 @@
+# Copyright (C) 2025 Rayness
+# This program is free software under GPLv3. See LICENSE for details.
+
 import os
 import sys
 import shutil
